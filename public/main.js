74e92b33a4fdde0166ef130bece60b80072f59ae
stian88 <rodriguezjs88@gmail.com>
2023-10-14 20:29:53 -0300

    update code
    update code

---
+++ public/main.js
@@ -0,0 +1,58 @@
+
+const selectCategory = document.getElementById("category");
+const selectLanguage = document.getElementById("language");
+const selectBlackFlags = document.getElementById("blackFlags");
+const selectType = document.getElementById("type");
+const selectAmount = document.getElementById("amount");
+const consultarButton = document.getElementById("consultar");
+const resultadoSingle= document.getElementById("single");
+const resultadoSetup = document.getElementById("setup");
+const resultadoDelivery = document.getElementById("delivery");
+
+       // Manejar el clic en el botón "Consultar API"
+consultarButton.addEventListener("click", function () {                        
+const opcionCategory = selectCategory.value;
+const opcionLanguage = selectLanguage.value;
+const opcionType = selectType.value
+const opcionAmount = selectAmount.value
+
+const checkboxesBlackFlags = selectBlackFlags.querySelectorAll('input[type="checkbox"]');
+const opcionBlackFlags = [];
+  
+checkboxesBlackFlags.forEach(checkbox => {
+   if(checkbox.checked){
+       opcionBlackFlags.push(checkbox.value)
+   }
+});
+
+consultarAPI(opcionCategory, opcionLanguage, opcionBlackFlags, opcionType, opcionAmount);
+
+   });
+
+ const apiUrl = 'https://v2.jokeapi.dev/joke/';
+
+   
+ function consultarAPI(category, language, blackflags, type, amount) {
+     fetch(apiUrl + category + language + blackflags + type + amount)
+        .then(response => {
+             if (!response.ok) {
+                   throw new Error('La solicitud no fue exitosa');
+               }
+               return response.json();
+           })
+        .then(data => {
+                
+                   resultadoSingle.textContent = JSON.stringify(data.joke);   
+                   console.log(data.joke);                     
+                   resultadoSetup.textContent = JSON.stringify(data.setup);
+                   console.log(data.setup);  
+                   resultadoDelivery.textContent = JSON.stringify( data.delivery);
+                   console.log(data.delivery);  
+                  
+                
+               // Hacer algo con los datos recibidos de la API
+           })
+        /*   .catch(error => {
+               resultadoDiv.textContent = 'Error al consultar la API: ' + error;
+           });*/
+   }
